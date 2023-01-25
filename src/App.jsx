57a9af86a1ccf9039ec5a4d2ee34f8974478f865
Christoffer <christoffer@christoffergylin.com>
2023-01-25 12:25:18 +0100

    Created functionality for localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InputBar from './InputBar'
 import List from './List';
 
 
 function App() {
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    console.log('useEffect on first render');
+
+    const dataFromStorage = JSON.parse(localStorage.getItem('data'));
+
+    if (dataFromStorage) {
+      console.log('dataFromStorage:');
+      console.log(dataFromStorage);
+      setData(dataFromStorage);
+    }
+
+  }, []);
+
+  useEffect(() => {
+
+    console.log('useEffect on update');
+
+    localStorage.setItem('data', JSON.stringify(data));
+
+  }, [data]);
 
   return (
     <div className="App flex flex-col w-full justify-start items-center">
